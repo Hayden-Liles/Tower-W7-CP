f1fd48c0dd7a520b73330317f0b1237c6ecb52f6
Hayden Liles <lileshaydenreal@gmail.com>
2023-03-01 13:20:53 -0700

    init

--- W7-CP.client/src/env.js
+++ W7-CP.client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'haydensdomain.us.auth0.com'
+export const clientId = 'cZ6bJgwJg7p3vIfjHgvC3GhyYPDfOUmD'
+export const audience = 'https://haydens-api.com'
